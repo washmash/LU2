f22584e18c1c7caa7f8ca6e4a20e1d5aba5fd580
LU2 <lsadfbnm@gmail.com>
2018-05-18 09:46:24 +0800

    main.c

--- main.c
+++ main.c
@@ -4,12 +4,12 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int IPone,SUM=0;
+	int IPone,SUM=0;/*宣告變數*/ 
 	for(IPone=0;IPone<10;IPone++)
 	{
-		SUM=SUM+IPone;
+		SUM=SUM+IPone;/*總和*/ 
 	}
-	printf("sum=%d",SUM);
+	printf("sum=%d",SUM);/*印出*/ 
 	
 	return 0;
 }
